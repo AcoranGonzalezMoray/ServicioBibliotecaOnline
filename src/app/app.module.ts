dca5e723243c6c3e64d2b1803e9949e84a198f61
santiago140103 <safloresreyna@gmail.com>
2023-03-13 14:00:34 +0000

    Firebase instalado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //Otras Importaciones
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -15,9 +26,21 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'serviciobibliotecaonline',
+    appId: '1:776665450263:web:551247c44513418138d952',
+    databaseURL: 'https://serviciobibliotecaonline-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'serviciobibliotecaonline.appspot.com',
+    apiKey: 'AIzaSyAcX4A_agogNYtrV1pXyH0XahHCf6KzEyQ',
+    authDomain: 'serviciobibliotecaonline.firebaseapp.com',
+    messagingSenderId: '776665450263',
+    measurementId: 'G-YJRSLD8ESL',
+  }
+};
