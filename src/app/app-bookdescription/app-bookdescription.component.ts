b27a6147082e1dda34285c9b4250e2f02e997afd
AcoranGonzalezMoray <acoranlol14@gmail.com>
2023-04-18 22:25:43 +0100

    Sprint 1 Acoran-Ivan Versión 1. Descripcion de Libros

--- src/app/app-bookdescription/app-bookdescription.component.ts
+++ src/app/app-bookdescription/app-bookdescription.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component} from '@angular/core';
 import { BookDescriptionService } from '../services/book-description.service';
 import { Book } from '../services/interfaces/book';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-app-bookdescription',
   templateUrl: './app-bookdescription.component.html',
@@ -9,11 +10,29 @@ import { Book } from '../services/interfaces/book';
 export class AppBookdescriptionComponent {
 
   public book: Book| undefined
-  constructor(bookDescriptionService:BookDescriptionService){
+  public id:string|undefined
+  public urlID:SafeResourceUrl|undefined
+  constructor(private sanitizer: DomSanitizer,bookDescriptionService:BookDescriptionService){
     this.book = bookDescriptionService.getLibro()
   }
 
+  leer(){
+    if( sessionStorage.getItem('user') &&this.book && this.book.url) {
+      // expresión regular para extraer el ID del archivo en la url
+      const pattern = /\/(file\/d\/|open\?id=)([^\/]*)/;
 
+      // extraer el ID del archivo de la URL utilizando la expresión regular
+      const match = this.book.url.match(pattern);
+      this.id = match ? match[2] : '';
+      // this.santier lo utilizamos para decir que la url ES SEGURA si no da error
+      //this.urlID se usa como src del ifrmae que esta en el html
+      this.urlID = this.sanitizer.bypassSecurityTrustResourceUrl('https://docs.google.com/file/d/'+this.id+'/preview')
+      
+    }
+  }
 
+  close(){
+    this.id= undefined
+  }
 
 }
