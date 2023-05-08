698f5e190e221e4777faa5e5019fcb203ab2dea6
AcoranGonzalezMoray <acoranlol14@gmail.com>
2023-05-08 20:56:06 +0100

    Sprint 2 Acoran-Santiago Version 4: AudioLibro terminado

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -22,14 +22,19 @@ interface AudioItem {
 
 export class AudioPlayerComponent implements OnInit {
   @Input() audioBook!: Audiobook 
-
   audioItems$!: Observable<AudioItem[]>;
   audioItems: AudioItem[] = [];
   audioItemsNoComplete: AudioItem[] = [];
   audioURL!: string;
+  element!: HTMLElement
+
+
+
+
   @ViewChild('audioPlayer') audioPlayer!: ElementRef;
-  
+  chapter: number = 0
   play = true
+  img!: AudioItem;
 
 
 
@@ -47,6 +52,7 @@ export class AudioPlayerComponent implements OnInit {
             nameSimple: nameSimple.split('_')[0]+nameSimple.split('_')[1],
             fullPath: item.fullPath
           };
+          this.chapter = parseInt(nameSimple.split('_')[1])
           return audioItem;
         });
       })
@@ -54,10 +60,22 @@ export class AudioPlayerComponent implements OnInit {
     this.audioItems$.subscribe((items: AudioItem[]) => {
       this.audioItems = items;
       this.audioItemsNoComplete =  items.filter(item => !item.name.endsWith('.jpg'));
+      this.img =  items.filter(item => item.name.endsWith('.jpg'))[0];
+      this.getImg(this.img.fullPath)
+      
+    });
+  }
+
+
+  getImg(fullPath: string){
+    const audioRef = this.storage.ref(fullPath);
+    audioRef.getDownloadURL().subscribe(url => {
+      this.img = url
     });
   }
 
   playAudio(filePath: string) {
+    this.chapter = parseInt(filePath.split('/')[1].split('_')[1])
     const audioRef = this.storage.ref(filePath);
     audioRef.getDownloadURL().subscribe(url => {
       this.audioURL = url;
@@ -65,6 +83,8 @@ export class AudioPlayerComponent implements OnInit {
       this.audioPlayer.nativeElement.play();
       this.play = true
     });
+
+    this.changeColor(filePath)
   }
 
   playStopAudioControl(){
@@ -76,6 +96,36 @@ export class AudioPlayerComponent implements OnInit {
       this.audioPlayer.nativeElement.pause();
       this.play = false
     } 
+  }
+
+
+  changeChapter(direccion: number) {
+    var minimo = parseInt(this.audioItemsNoComplete[0].name.split('_')[1])
+    if(this.chapter>minimo && this.chapter<this.audioItemsNoComplete.length){
+      this.chapter = this.chapter+direccion
+      var audio:AudioItem = this.audioItemsNoComplete[this.chapter-1]
+      this.playAudio(audio.fullPath)  
+    }
+  }
+
+  changeColor(id:string){
+    if(this.element) this.element.style.display = "none"
+    this.element = document.getElementById(id)!
+    this.element.style.display = "block"
+  }
+
+  viewChapters(id:string){
+    var chapter = document.getElementById(id)!
+    var element = document.getElementById(id+"Des")!
+    if(chapter .style.display !="block"){
+      chapter.style.display="block"
+      element.style.maxWidth="250px"
+      element.style.fontSize="14px"
+    }else{
+      chapter .style.display="none"
+      element.style.maxWidth="500px"
+      element.style.fontSize="20px"
+    }
   }
   
 }
